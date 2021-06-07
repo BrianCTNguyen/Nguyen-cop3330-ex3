a32d20747bdd33a44ec01749e4cd0e00d8061ffb
Brian Nguyen <BrianCTNguyen@knights.ucf.edu>
2021-06-06 21:33:19 -0400

    Initial solution problem 3

--- src/main/java/base/main.java
+++ src/main/java/base/main.java
@@ -3,7 +3,7 @@ package base;
 import java.util.Scanner;
 
 /*
- *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  UCF COP3330 Summer 2021 Assignment 3 Solution
  *  Copyright 2021 Brian Nguyen
  */
 
